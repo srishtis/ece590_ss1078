05be43838cae8457b67a03510686f368e7a2f4df
ss1078 <srishti.saha@duke.edu>
2020-02-02 21:43:23 -0500

    added files

---
+++ main.py
@@ -0,0 +1,72 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_flex_quickstart]
+import logging
+
+from flask import Flask
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+    
+@app.route('/<size>')
+def printTriangle(size):
+    # Start with starCount being 0.
+    starCount = 0
+    # Count from 0 (inclusive) to size (exclusive),
+    # and for each number i that you count,
+    size=int(size)
+    s=""
+    for i in range(0, size):
+        # count from 0 (inclusive) to i (inclusive),
+        # and for each number j that you count,
+        for j in range(0, i+1):
+            # print a "*" without printing a newline,
+            s=s+ "*"
+            # increment starCount.
+            starCount += 1
+            # When you finish counting on j,
+
+        # print a newline ("\n").
+        s=s+ '<br/>'
+        # When you finish counting on i,
+
+    # your answer is starCount.
+    return s
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_flex_quickstart]
